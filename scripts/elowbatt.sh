84e3b179a31a44b136adec5834f44c17e0809942
Alexander Kerner <lunohod@openinkpot.org>
2009-06-12 23:55:03 +0200

    fixed mdev script

--- scripts/elowbatt.sh
+++ scripts/elowbatt.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-set -e
+#set -e
 
 . /etc/default/elowbatt
 
 if [ x"$ACTION" = x"change" \
 	   -a x"$POWER_SUPPLY_TYPE" = x"Battery" \
 	   -a x"$POWER_SUPPLY_STATUS" = x"Discharging" \
-	   -a $POWER_SUPPLY_CHARGE -le $CHARGE_LEVEL_ALARM ]
+	   -a 0"$POWER_SUPPLY_CHARGE_NOW" -le 0"$CHARGE_LEVEL_ALARM" ]	 
 then
 	uk-send elowbatt "LOWBATT"
 fi
